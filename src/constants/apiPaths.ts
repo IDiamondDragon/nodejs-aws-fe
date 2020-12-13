2d7ccf8ae202cf59ab1be51271e1596c7825e9d4
Michael Markeev <michael.markeev@rokolabs.com>
2020-12-13 14:32:03 +0400

    backend for frontend

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,13 @@
 
 const API_PATHS = {
-  product: 'https://ppauvpo36j.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'http://idiamonddragon-bff-api-develop.eu-west-1.elasticbeanstalk.com',
+  cart: 'http://idiamonddragon-bff-api-develop.eu-west-1.elasticbeanstalk.com',
   order: 'https://ppauvpo36j.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://40gb9qlpj5.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://ppauvpo36j.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://idiamonddragon-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
+  bff: 'http://idiamonddragon-bff-api-develop.eu-west-1.elasticbeanstalk.com',
+  // bff: 'https://ppauvpo36j.execute-api.eu-west-1.amazonaws.com/dev',
+  // product: 'https://ppauvpo36j.execute-api.eu-west-1.amazonaws.com/dev',
+  // cart: 'http://idiamonddragon-cart-api-dev.eu-west-1.elasticbeanstalk.com/api/profile',
 };
 
 export default API_PATHS;
